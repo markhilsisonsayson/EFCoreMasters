26199b4ee0649d820d3a1b47ef494f48970171d8
markhilsisonsayson <111505309+markhilsisonsayson@users.noreply.github.com>
2022-11-15 23:40:51 +0800

    Session 2 Assignment

---
+++ Session2/EFCoreAssignment/Entities/Review.cs
@@ -0,0 +1,13 @@
+﻿namespace EFCoreAssignment.Entities
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+
+        public Product Product { get;set; }
+        public string ReviewerName { get; set; }
+        public string Comment { get; set; }
+        public byte NumberOfStars { get; set; }
+    }
+}
