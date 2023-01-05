6960ba109a9ad90f57b09585b44533b924dad355
markhilsisonsayson <111505309+markhilsisonsayson@users.noreply.github.com>
2023-01-05 12:18:52 +0800

    Session 7 Assignment

---
+++ Session7/InventoryAppEFCore.DataLayer/Migrations/20230105040541_AddVW_Products.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryAppEFCore.DataLayer.Migrations
+{
+    public partial class AddVW_Products : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            create view dbo.VW_Products as
+            select ProductId,Name from Products;
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            drop view dbo.VW_Products;
+            ");
+        }
+    }
+}
---
+++ Session7/InventoryAppEFCore.DataLayer/Profiles/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using InventoryAppEFCore.DataLayer.DTOs;
+using InventoryAppEFCore.DataLayer.EfClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryAppEFCore.DataLayer.Profiles
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile() {
+            CreateMap<Client, ClientDTO>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => $"{src.Name}"))
+                .ReverseMap();
+        }
+    }
+}
